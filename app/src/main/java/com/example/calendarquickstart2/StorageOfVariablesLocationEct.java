d42da4393810545b1685c4a8850e6458c94ff9c8
munkefrugt@gmail.com <munkefrugt@gmail.com>
2016-05-05 16:13:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/calendarquickstart2/StorageOfVariablesLocationEct.java
@@ -0,0 +1,13 @@
+package com.example.calendarquickstart2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StorageOfVariablesLocationEct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storage_of_variables_location_ect);
+    }
+}
