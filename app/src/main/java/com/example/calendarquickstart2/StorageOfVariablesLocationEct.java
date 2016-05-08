f649c624a13fdaa78715517e1ff66424985125d5
munkefrugt@gmail.com <munkefrugt@gmail.com>
2016-05-08 17:10:46 +0200

    nu kan trykke på fab og så gemmer den oplysninger i storage

--- app/src/main/java/com/example/calendarquickstart2/StorageOfVariablesLocationEct.java
+++ app/src/main/java/com/example/calendarquickstart2/StorageOfVariablesLocationEct.java
@@ -29,8 +29,9 @@ public class StorageOfVariablesLocationEct extends AppCompatActivity {
 
 
 
-        // get the data:
+        // get the data from main class:
         Bundle extras = getIntent().getExtras();
+        // hvis der noget..
         if (extras != null) {
             location = extras.getString("Location String");
             Log.i("location",location);
@@ -38,14 +39,39 @@ public class StorageOfVariablesLocationEct extends AppCompatActivity {
             //Log.i("arrivalTime",arrivalTime);
 
 
-            TextView view = (TextView) findViewById(R.id.storage);
+            TextView view = (TextView) findViewById(R.id.location);
             view.setText(location);
 
-            TextView view1 = (TextView) findViewById(R.id.time);
-            view1.setText(eventStartTime);
+
         }
-        /*TextView storageView = (TextView) findViewById(R.id.storage);
-        storageView.setText(travelLocation);
-           */
+        // Now clean the data:
+        extractTheTime();
+
+    }
+    // here we split the DateTime type to something usefull.
+    private void extractTheTime() {
+
+        String string = eventStartTime;//"2016-05-04T14:00:00.000+02:00";
+
+
+        String[] parts = string.split(".000");
+        String part1 = parts[0];
+        String part2 = parts[1];
+
+        //System.out.println("part1  "+part1);
+        //System.out.println("part2  "+ part2);
+
+        String[] splitpart1 = part1.split("T");
+
+        String date = splitpart1[0];
+        String theTime = splitpart1[1];
+
+
+        Log.i("extractTheTime: ", theTime);
+        Log.i("extractTheTime: ", date);
+        eventStartTime =theTime;
+
+        TextView view1 = (TextView) findViewById(R.id.time);
+        view1.setText(eventStartTime);
     }
 }
